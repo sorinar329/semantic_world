4943603aaffe3a0621ae80f42406579df85d1bf7
sorinar329 <sorin@uni-bremen.de>
2025-06-03 16:07:02 +0200

    changed test_fridge_view to new format

--- src/semantic_world/views/world_rdr/world_views_mcrdr.py
+++ src/semantic_world/views/world_rdr/world_views_mcrdr.py
@@ -1,15 +1,15 @@
 from ripple_down_rules.datastructures.case import Case, create_case
+from typing_extensions import Optional, Set
 from ripple_down_rules.utils import make_set
-from typing import Set, Union
 from .world_views_mcrdr_defs import *
 
 
 attribute_name = 'views'
-conclusion_type = (Door, Drawer, set, Fridge, list, Cabinet, Container, Handle,)
+conclusion_type = (Drawer, Fridge, set, Cabinet, list, Container, Handle, Door,)
 mutually_exclusive = False
 
 
-def classify(case: World, **kwargs) -> Set[Union[Door, Drawer, Fridge, Cabinet, Container, Handle]]:
+def classify(case: World, **kwargs) -> Set[Union[Drawer, Fridge, Cabinet, Container, Handle, Door]]:
     if not isinstance(case, Case):
         case = create_case(case, max_recursion_idx=3)
     conclusions = set()
