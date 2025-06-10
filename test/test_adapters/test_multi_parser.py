e45f04808c25e176e525e6d0a20751c76149b737
Giang Nguyen <hoanggia@uni-bremen.de>
2025-06-10 12:39:35 +0200

    Add multi_parser

---
+++ test/test_adapters/test_multi_parser.py
@@ -0,0 +1,50 @@
+import os.path
+import unittest
+
+from semantic_world.adapters.multi_parser import MultiParser
+from semantic_world.connections import FixedConnection
+
+
+class MultiParserTestCase(unittest.TestCase):
+    urdf_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "resources", "urdf")
+    table = os.path.join(urdf_dir, "table.urdf")
+    kitchen = os.path.join(urdf_dir, "kitchen-small.urdf")
+    apartment = os.path.join(urdf_dir, "apartment.urdf")
+    pr2 = os.path.join(urdf_dir, "pr2_kinematic_tree.urdf")
+
+    def setUp(self):
+        self.table_parser = MultiParser(self.table)
+        self.kitchen_parser = MultiParser(self.kitchen)
+        self.apartment_parser = MultiParser(self.apartment)
+        self.pr2_parser = MultiParser(self.pr2)
+
+    def test_table_parsing(self):
+        world = self.table_parser.parse()
+        world.validate()
+        self.assertEqual(len(world.bodies), 6)
+
+        origin_left_front_leg_joint = world.get_connection(world.root, world.bodies[1])
+        self.assertIsInstance(origin_left_front_leg_joint, FixedConnection)
+
+    def test_kitchen_parsing(self):
+        world = self.kitchen_parser.parse()
+        world.validate()
+        self.assertTrue(len(world.bodies) > 0)
+        self.assertTrue(len(world.connections) > 0)
+
+    def test_apartment_parsing(self):
+        world = self.apartment_parser.parse()
+        world.validate()
+        self.assertTrue(len(world.bodies) > 0)
+        self.assertTrue(len(world.connections) > 0)
+
+    def test_pr2_parsing(self):
+        world = self.pr2_parser.parse()
+        world.validate()
+        self.assertTrue(len(world.bodies) > 0)
+        self.assertTrue(len(world.connections) > 0)
+        self.assertTrue(world.root.name.name == 'base_footprint')
+
+
+if __name__ == '__main__':
+    unittest.main()
