ceac0a713fb7842ec2fbcfbe08b2469a78aa3619
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-06-30 11:36:58 +0200

    Fixed ORM.

--- src/semantic_world/views/world_rdr/world_views_mcrdr.py
+++ src/semantic_world/views/world_rdr/world_views_mcrdr.py
@@ -1,15 +1,15 @@
+from typing_extensions import Optional, Set
 from ripple_down_rules.datastructures.case import Case, create_case
 from ripple_down_rules.utils import make_set
-from typing_extensions import Optional, Set
 from .world_views_mcrdr_defs import *
 
 
 attribute_name = 'views'
-conclusion_type = (Fridge, Container, set, list, Door, Cabinet, Drawer, Handle,)
+conclusion_type = (set, Drawer, list, Fridge, Container, Handle, Cabinet, Door,)
 mutually_exclusive = False
 
 
-def classify(case: World, **kwargs) -> Set[Union[Fridge, Container, Door, Cabinet, Drawer, Handle]]:
+def classify(case: World, **kwargs) -> Set[Union[Drawer, Fridge, Container, Handle, Cabinet, Door]]:
     if not isinstance(case, Case):
         case = create_case(case, max_recursion_idx=3)
     conclusions = set()
