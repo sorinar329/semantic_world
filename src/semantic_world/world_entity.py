03c538001c35ab41bc0b8203a6b9349200791b4d
Luca Krohm <luc_kro@uni-bremen.de>
2025-07-07 22:14:12 +0200

    [body and view] added bb collection utility methods for both and added aggregated_bodies property for views

--- src/semantic_world/world_entity.py
+++ src/semantic_world/world_entity.py
@@ -1,11 +1,16 @@
 from __future__ import annotations
 
+from collections import deque
+from collections.abc import Iterable
 from dataclasses import dataclass, field
-from typing import List, Optional, TYPE_CHECKING
+from functools import reduce
+from typing import List, Optional, TYPE_CHECKING, Set
+import numpy as np
+from numpy import ndarray
 
-from .geometry import Shape, BoundingBox
+from .geometry import Shape, BoundingBox, BoundingBoxCollection
 from .prefixed_name import PrefixedName
-from .spatial_types.spatial_types import TransformationMatrix, Expression
+from .spatial_types.spatial_types import TransformationMatrix, Expression, Point3
 from .spatial_types import spatial_types as cas
 from .utils import IDGenerator
 
@@ -79,12 +84,41 @@ class Body(WorldEntity):
         return len(self.collision) > 0
 
     @property
-    def bounding_box(self) -> BoundingBox:
+    def bounding_box_collection(self) -> BoundingBoxCollection:
         """
-        Returns the bounding box of the body if it exists, otherwise returns None.
-        """
-        return self.collision[0].as_bounding_box()
+        Return the bounding box collection of the link with the given name.
+        This method computes the bounding box of the link in world coordinates by transforming the local axis-aligned
+        bounding boxes of the link's geometry to world coordinates.
 
+        :return: A BoundingBoxCollection containing the bounding boxes of the link's geometry in world
+        """
+        world = self._world
+        body_transform: ndarray = world.compute_forward_kinematics_np(world.root, self)
+        world_bboxes = []
+
+        for shape in self.collision:
+            shape_transform: ndarray = shape.origin.to_np()
+
+            world_transform: ndarray = body_transform @ shape_transform
+            body_pos = world_transform[:3, 3]
+            body_rotation_matrix = world_transform[:3, :3]
+
+            local_bb: BoundingBox = shape.as_bounding_box()
+
+            # Get all 8 corners of the BB in link-local space
+            corners = np.array([corner.to_np()[:3] for corner in local_bb.get_points()])  # shape (8, 3)
+
+            # Transform each corner to world space: R * corner + T
+            transformed_corners = (corners @ body_rotation_matrix.T) + body_pos
+
+            # Compute world-space bounding box from transformed corners
+            min_corner = np.min(transformed_corners, axis=0)
+            max_corner = np.max(transformed_corners, axis=0)
+
+            world_bb = BoundingBox.from_min_max(Point3.from_xyz(*min_corner), Point3.from_xyz(*max_corner))
+            world_bboxes.append(world_bb)
+
+        return BoundingBoxCollection(world_bboxes)
 
 
 class View(WorldEntity):
@@ -94,6 +128,45 @@ class View(WorldEntity):
     This class can hold references to certain bodies that gain meaning in this context.
     """
 
+    @property
+    def aggregated_bodies(self) -> Set[Body]:
+        """
+        Recursively traverses the view and its attributes to find all bodies contained within it.
+
+        :return: A set of bodies that are part of this view.
+        """
+        bodies: Set[Body] = set()
+        visited: Set[int] = set()
+        stack: deque = deque([self])
+
+        while stack:
+            obj = stack.pop()
+            oid = id(obj)
+            if oid in visited:
+                continue
+            visited.add(oid)
+
+            if isinstance(obj, Body):
+                bodies.add(obj)
+                continue
+
+            if isinstance(obj, View):
+                stack.extend(obj.__dict__.values())
+                continue
+
+            if isinstance(obj, Iterable) and not isinstance(obj, (str, bytes, bytearray)):
+                stack.extend(obj)
+        return bodies
+
+    def as_bounding_box_collection(self) -> BoundingBoxCollection:
+        """
+        Returns a bounding box collection that contains the bounding boxes of all bodies in this view.
+        """
+        bbs = reduce(
+            lambda accumulator, bb_collection: accumulator.merge(bb_collection),
+            (body.bounding_box_collection for body in self.aggregated_bodies if body.has_collision())
+        )
+        return bbs
 
 @dataclass
 class Connection(WorldEntity):
