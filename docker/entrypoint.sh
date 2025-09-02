3b694653aa6939ca18374d97097503b68093968e
Jonas Dech <j.dech@gmx.net>
2025-09-02 14:54:44 +0200

    New venv for Multiverse-parser

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,8 @@ set -e
 source /opt/ros/overlay_ws/install/setup.bash
 
 cd /opt/ros/
-python3 -m venv semantic_world-venv --system-site-packages  && source semantic_world-venv/bin/activate && pip install -U pip && pip install -U setuptools && pip install -r /opt/ros/overlay_ws/src/semantic_world/requirements.txt
+python3 -m venv semantic_world-venv --system-site-packages  && source semantic_world-venv/bin/activate && pip install -U pip && pip install -U setuptools && pip install -r /opt/ros/overlay_ws/src/semantic_world/requirements.txt && pip install -e Multiverse-Parser
+
 
 source /opt/ros/semantic_world-venv/bin/activate
 
