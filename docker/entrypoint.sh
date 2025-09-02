17eb7ac0c19491f6aa2d6159cc70e59086e65af0
Jonas Dech <j.dech@gmx.net>
2025-09-02 14:49:03 +0200

    Moved venv creation to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,10 @@
 set -e
 
 source /opt/ros/overlay_ws/install/setup.bash
+
+cd /opt/ros/
+python3 -m venv semantic_world-venv --system-site-packages  && source semantic_world-venv/bin/activate && pip install -U pip && pip install -U setuptools && pip install -r /opt/ros/overlay_ws/src/semantic_world/requirements.txt
+
 source /opt/ros/semantic_world-venv/bin/activate
 
 exec "$@"
