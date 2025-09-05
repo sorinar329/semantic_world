b867ca83bd6e0b2ff03601cd447d1f5228cd5663
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-09-05 12:44:01 +0200

    Test for getting the root

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,2 +1,9 @@
+import os
+
+from semantic_world.utils import get_semantic_world_directory_root
+
+
 def test_get_semantic_world_directory_root():
-    path = "home/user/repos/semantic_world/semantic_world"
+    path = os.path.abspath(__file__)
+    root = get_semantic_world_directory_root(path)
+    assert root == os.path.abspath(os.path.join(os.path.dirname(path), ".."))
